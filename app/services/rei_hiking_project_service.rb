b815d119163bf9f7513f04d27bef13418d85b801
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-10-23 22:38:14 -0600

    Add test for rei hiking project service, add rei hiking project service

---
+++ app/services/rei_hiking_project_service.rb
@@ -0,0 +1,16 @@
+class ReiHikingProjectService
+  def initialize
+    @conn = Faraday.new(url: "https://www.hikingproject.com")
+  end
+
+  def trails(latitude, longitude)
+    get_url("/get-trails?lat=#{latitude}&lon=-#{longitude}&maxDistance=10&key=#{ENV["HIKING_PROJECT_KEY"]}")
+  end
+
+  private
+
+  def get_url(url)
+    response = @conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
--- spec/services/rei_hiking_project_service.rb
+++ spec/services/rei_hiking_project_service.rb
@@ -1,68 +1,26 @@
 require "rails_helper"
 
-require "rails_helper"
-
-describe GooglePlaceService do
+describe ReiHikingProjectService do
   describe "instance methods" do
-    describe "#locations" do
-      it "returns all places from relevant homeless shelter search" do
-        VCR.use_cassette("google_place_service_homeless_shelter_locations") do
-          gps = GooglePlaceService.new
-          type = "homeless_shelter"
-          location = "Denver"
+    describe "#trails" do
+      it "returns all trails within a certain distance from state park" do
+        VCR.use_cassette("rei_hiking_project_service_trails") do
+          rhps = ReiHikingProjectService.new
+          latitude = 39.3379
+          longitude = -104.7512
 
-          locations = gps.locations(type, location)
+          trails = rhps.trails(latitude, longitude)
+          trail = trails.first
 
-          location = locations.first
-
-          expect(location).to have_key(:result)
-          expect(location[:result]).to have_key(:formatted_address)
-          expect(location[:result]).to have_key(:formatted_phone_number)
-          expect(location).to have_key(:location)
-        end
-      end
-
-      it "returns all places from relevant library search" do
-        VCR.use_cassette("google_place_service_library_locations") do
-          gps = GooglePlaceService.new
-          type = "library"
-          location = "Denver"
-
-          locations = gps.locations(type, location)
-          location = locations.first
-
-          expect(location).to have_key(:result)
-          expect(location[:result]).to have_key(:formatted_address)
-          expect(location[:result]).to have_key(:formatted_phone_number)
-        end
-      end
-
-      it "returns all places from relevant food search" do
-        VCR.use_cassette("google_place_free_food_locations") do
-          gps = GooglePlaceService.new
-          type = "food+for+homeless"
-          location = "Denver"
-
-          locations = gps.locations(type, location)
-          location = locations.first
-
-          expect(location).to have_key(:result)
-          expect(location[:result]).to have_key(:formatted_address)
-          expect(location[:result]).to have_key(:formatted_phone_number)
-        end
-      end
-
-      it "returns all places from relevant crisis search" do
-        VCR.use_cassette("google_place_crisis_center_locations") do
-          gps = GooglePlaceService.new
-          type = "crisis+service"
-          location = "Denver"
-
-          locations = gps.locations(type, location)
-          location = locations.first
-
-          expect(location).to have_key(:result)
-          expect(location[:result]).to have_key(:formatted_address)
+          expect(trail).to have_key(:trails)
+          expect(trail[:trails]).to have_key(:id)
+          expect(trail[:trails]).to have_key(:difficulty)
+          expect(trail[:trails]).to have_key(:length)
+          expect(trail[:trails]).to have_key(:status)
+          expect(trail[:trails]).to have_key(:imgMedium)
+          expect(trail[:trails]).to have_key(:summary)
+          expect(trail[:trails]).to have_key(:latitude)
+          expect(trail[:trails]).to have_key(:longitude)
         end
       end
     end
