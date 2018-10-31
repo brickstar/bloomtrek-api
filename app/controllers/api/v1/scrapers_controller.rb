72d6eac267a6174bf75b238af9c3c0cbace75e30
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-10-30 18:05:38 -0600

    Add scraper presenter to write CSV files, refactor scraper controller

--- app/controllers/api/v1/scrapers_controller.rb
+++ app/controllers/api/v1/scrapers_controller.rb
@@ -3,11 +3,7 @@ class Api::V1::ScrapersController < ApplicationController
   before_action :set_website, only: [:index]
 
   def index
-    # response = Faraday.get("https://bloomtrek-api-scraper.herokuapp.com/api/v1/scraper?website=#{@website}")
-    scraper = ScraperService.new
-    flowers = scraper.flowers(@website)
-    writer = CsvWriter.new(params[:csv_file_name])
-    writer.write_csv(flowers)
+    CsvFlowerPresenter.new(@website, params[:csv_file_name]).generate_flower_csv
   end
 
   private
