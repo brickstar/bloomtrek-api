ac74e93f36f320a338f791920e2221efd0c6e6f0
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-10-30 18:42:39 -0600

    [#161590045]Add TrailWorker tests

--- spec/workers/trail_worker_spec.rb
+++ spec/workers/trail_worker_spec.rb
@@ -4,9 +4,4 @@ require 'rails_helper'
 describe TrailWorker do
   it { is_expected.to be_processed_in :default }
   it { is_expected.to be_retryable false }
-
-  it 'enqueues another awesome job' do
-    TrailWorker.perform_async 'Awesome', true
-    expect(TrailWorker).to have_enqueued_sidekiq_job('Awesome', true)
-  end
 end
