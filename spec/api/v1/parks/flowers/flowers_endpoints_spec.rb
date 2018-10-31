7a7c99a5699a861f1965bdfd50c6e00059a98192
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-10-31 13:53:12 -0600

    Refactor flower tests to nested folder in parks

---
+++ spec/api/v1/parks/flowers/flowers_endpoints_spec.rb
@@ -0,0 +1,56 @@
+require "rails_helper"
+
+describe "Flowers API" do
+  before(:each) do
+    @parks = create_list(:park, 3)
+    @park = @parks.first
+    @trail1 = create(:trail, park_id: @park.id, difficulty: "green")
+    @trail2 = create(:trail, park_id: @park.id, difficulty: "black")
+    @trail3 = create(:trail, park_id: @park.id)
+    @flower1 = create(:flower)
+    @flower2 = create(:flower)
+    @flower3 = create(:flower)
+    ParkFlower.create(park_id: @park.id, flower_id: @flower1.id)
+    ParkFlower.create(park_id: @park.id, flower_id: @flower2.id)
+  end
+
+  describe "GET /api/v1/parks/:id/flowers" do
+    it "sends flowers if month passed in is between start and end bloom" do
+      get "/api/v1/parks/#{@parks.first.id}/flowers", params: { :month => 6 }
+      expect(response).to be_successful
+
+      flowers = JSON.parse(response.body, symbolize_names: true)
+
+      expect(flowers).to be_a(Array)
+      expect(flowers.length).to eq(2)
+      expect(flowers[0]).to have_key(:id)
+      expect(flowers[0]).to have_key(:common_name)
+      expect(flowers[0]).to have_key(:scientific_name)
+      expect(flowers[0]).to have_key(:flower_img_url)
+      expect(flowers[0]).to have_key(:description)
+      expect(flowers[0]).to have_key(:bloom_start)
+      expect(flowers[0]).to have_key(:bloom_end)
+      expect(flowers[0]).to have_key(:name)
+      expect(flowers[0]).to have_key(:habitat)
+    end
+
+    it "does not send flowers if month passed in is NOT between start and end bloom" do
+      get "/api/v1/parks/#{@parks.first.id}/flowers", params: { :month => 12 }
+      expect(response).to be_successful
+
+      flowers = JSON.parse(response.body, symbolize_names: true)
+
+      expect(flowers.length).to eq(0)
+    end
+
+    it "sends a 404 if park is not found for flowers" do
+      get "/api/v1/parks/100/flowers"
+      expect(response.status).to eq(404)
+    end
+
+    it "sends a 404 if park is not found for trails" do
+      get "/api/v1/parks/100/trails"
+      expect(response.status).to eq(404)
+    end
+  end
+end
--- spec/api/v1/parks/parks_endpoint_spec.rb
+++ spec/api/v1/parks/parks_endpoint_spec.rb
@@ -47,42 +47,4 @@ describe "Parks API" do
     expect(park[0][:coords]).to have_key(:longitude)
     expect(park.count).to eq(3)
   end
-
-  it "sends a single park and its associated flowers if month passed in is between start and end bloom" do
-    get "/api/v1/parks/#{@parks.first.id}/flowers", params: { :month => 6 }
-    expect(response).to be_successful
-
-    flowers = JSON.parse(response.body, symbolize_names: true)
-
-    expect(flowers).to be_a(Array)
-    expect(flowers.length).to eq(2)
-    expect(flowers[0]).to have_key(:id)
-    expect(flowers[0]).to have_key(:common_name)
-    expect(flowers[0]).to have_key(:scientific_name)
-    expect(flowers[0]).to have_key(:flower_img_url)
-    expect(flowers[0]).to have_key(:description)
-    expect(flowers[0]).to have_key(:bloom_start)
-    expect(flowers[0]).to have_key(:bloom_end)
-    expect(flowers[0]).to have_key(:name)
-    expect(flowers[0]).to have_key(:habitat)
-  end
-
-  it "does not send flowers if month passed in is NOT between start and end bloom" do
-    get "/api/v1/parks/#{@parks.first.id}/flowers", params: { :month => 12 }
-    expect(response).to be_successful
-
-    flowers = JSON.parse(response.body, symbolize_names: true)
-
-    expect(flowers.length).to eq(0)
-  end
-
-  it "sends a 404 if park is not found for flowers" do
-    get "/api/v1/parks/100/flowers"
-    expect(response.status).to eq(404)
-  end
-
-  it "sends a 404 if park is not found for trails" do
-    get "/api/v1/parks/100/trails"
-    expect(response.status).to eq(404)
-  end
 end
