48ae5d35b6f2c7fe219f8d3c87bdb6f2e2e38932
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-10-31 18:58:05 -0600

    [#161639140]refactors flowers, parks, and trails controllers

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::TrailsController < ApplicationController
   before_action :validate_park, only: [:index]
+  before_action :set_trails, only: [:index]
 
   def index
-    park = Park.find_by(id: params[:park_id])
-    render json: park.trails
+    render json: @trails
+  end
+
+  def set_trails
+    @trails = Park.find_by(id: params[:park_id]).trails
   end
 end
